e857ba02cc1f22d586c4601d82c7a06841792ad8
Golde34 <nguyendongducviet2001@gmail.com>
2023-12-28 22:30:36 +0700

    add env config

--- middleware/api/service_api/auth_service.go
+++ middleware/api/service_api/auth_service.go
@@ -1,11 +1,11 @@
 // Get API of auth service put to GraphQL Schema
 
-package api
+package service_api
 
 import (
 	"context"
 	"log"
-	
+
 	"github.com/joho/godotenv"
 )
 
@@ -13,7 +13,7 @@ type AuthService interface {
 	Auth(ctx context.Context, username string, password string) (string, error)
 }
 
-type authService struct {}
+type authService struct{}
 
 func NewAuthService() AuthService {
 	return &authService{}
@@ -27,39 +27,39 @@ func (a *authService) Auth(ctx context.Context, username string, password string
 	log.Printf("Username: %s", username)
 	return username, nil
 	// authUrl := fmt.Sprintf("%s/auth", os.Getenv("AUTH_SERVICE_URL"))
-	
+
 	// data := map[string]string{
 	// 	"username": username,
 	// 	"password": password,
 	// }
-	
+
 	// payload, err := json.Marshal(data)
 	// if err != nil {
 	// 	return "", err
 	// }
-	
+
 	// req, err := http.NewRequest("POST", authUrl, bytes.NewBuffer(payload))
 	// if err != nil {
 	// 	return "", err
 	// }
-	
+
 	// req.Header.Set("Content-Type", "application/json")
-	
+
 	// client := &http.Client{}
 	// resp, err := client.Do(req)
 	// if err != nil {
 	// 	return "", err
 	// }
-	
+
 	// defer resp.Body.Close()
-	
+
 	// body, err := ioutil.ReadAll(resp.Body)
 	// if err != nil {
 	// 	return "", err
 	// }
-	
+
 	// var result map[string]interface{}
 	// json.Unmarshal([]byte(body), &result)
-	
+
 	// return result["token"].(string), nil
 }
